af5a8ccc90c38476ce3ef05111c896b1771dd2cd
aaggarwal_fwinc <amit.aggarwal@freshworks.com>
2026-05-26 14:59:47 +0530

    working on join  test cases

--- src/test/java/com/freshworks/core/processor/joins/TestLeftJoinService.java
+++ src/test/java/com/freshworks/core/processor/joins/TestLeftJoinService.java
@@ -21,7 +21,6 @@ import com.freshworks.core.data.four_zero_zero.unit.processor.joins.beans.FbUsag
 import com.freshworks.core.data.four_zero_zero.unit.processor.joins.beans.FbUserBean;
 import com.freshworks.core.processor.AbstractBean;
 import com.freshworks.core.processor.Annotations.FreshJoin;
-import com.freshworks.core.processor.joins.LeftJoinService;
 
 @SpringBootTest
 @EnabledIfSystemProperty(named = "spring.profiles.active", matches = ".*\\.unit\\..*")
@@ -66,40 +65,40 @@ public class TestLeftJoinService {
 
     }
 
-    @Test
-    public void testCompareParent() throws Exception{
-
-        ObjectMapper objectMapper = new ObjectMapper();
-
-        FbApplicationBean fbApplicationBean = new FbApplicationBean();
-        fbApplicationBean.setApplicationId("app_id_994433");
-        fbApplicationBean.setApplicationName("slack");
-
-        FbServicePrincipleBean fbServicePrincipleBean = new FbServicePrincipleBean();
-        fbServicePrincipleBean.setServicePrincipleId("sp_id_5575775");
-        fbServicePrincipleBean.setParentBean(objectMapper.convertValue(fbApplicationBean, JsonNode.class));
-
-
-        FbUserBean fbUserBean = new FbUserBean();
-        fbUserBean.setLastName("aggarwal");
-        fbUserBean.setFirstName("amit");
-        fbUserBean.setId("user_id_123456");
-        fbUserBean.setParentBean(objectMapper.convertValue(fbServicePrincipleBean, JsonNode.class));
-
-        FbUsageBean fbUsageBean = new FbUsageBean();
-        fbUsageBean.setCreatedAt("02-06-1989");
-        fbUsageBean.setUserId("user_id_654321");
-        fbUsageBean.setParentBean(objectMapper.convertValue(fbApplicationBean, JsonNode.class));
-
-        ArrayList<AbstractBean> list = new ArrayList<>();
-        list.add(0, fbUserBean);
-        HashMap<String, AbstractBean> unwrappedLeftClassMap = leftJoinService.unwrappedBeanToClassMap(list);
+    // @Test
+    // public void testCompareParent() throws Exception{
         
-        list.add(0, fbUsageBean);
-        HashMap<String, AbstractBean> unwrappedRightClassMap = leftJoinService.unwrappedBeanToClassMap(list);
+    //     ObjectMapper objectMapper = new ObjectMapper();
 
-        Boolean isEqual = leftJoinService.compareParent(unwrappedLeftClassMap, unwrappedRightClassMap);
-        assertThat(isEqual, Matchers.is(true));
+    //     FbApplicationBean fbApplicationBean = new FbApplicationBean();
+    //     fbApplicationBean.setApplicationId("app_id_994433");
+    //     fbApplicationBean.setApplicationName("slack");
 
-    }
+    //     FbServicePrincipleBean fbServicePrincipleBean = new FbServicePrincipleBean();
+    //     fbServicePrincipleBean.setServicePrincipleId("sp_id_5575775");
+    //     fbServicePrincipleBean.setParentBean(objectMapper.convertValue(fbApplicationBean, JsonNode.class));
+
+
+    //     FbUserBean fbUserBean = new FbUserBean();
+    //     fbUserBean.setLastName("aggarwal");
+    //     fbUserBean.setFirstName("amit");
+    //     fbUserBean.setId("user_id_123456");
+    //     fbUserBean.setParentBean(objectMapper.convertValue(fbServicePrincipleBean, JsonNode.class));
+
+    //     FbUsageBean fbUsageBean = new FbUsageBean();
+    //     fbUsageBean.setCreatedAt("02-06-1989");
+    //     fbUsageBean.setUserId("user_id_654321");
+    //     fbUsageBean.setParentBean(objectMapper.convertValue(fbApplicationBean, JsonNode.class));
+
+    //     ArrayList<AbstractBean> list = new ArrayList<>();
+    //     list.add(0, fbUserBean);
+    //     HashMap<String, AbstractBean> unwrappedLeftClassMap = leftJoinService.unwrappedBeanToClassMap(list);
+        
+    //     list.add(0, fbUsageBean);
+    //     HashMap<String, AbstractBean> unwrappedRightClassMap = leftJoinService.unwrappedBeanToClassMap(list);
+
+    //     Boolean isEqual = leftJoinService.compareParent(unwrappedLeftClassMap, unwrappedRightClassMap);
+    //     assertThat(isEqual, Matchers.is(true));
+
+    // }
 }
