0176c14cf317a567bd621031a9b4963c3dab3bfc
aaggarwal_fwinc <amit.aggarwal@freshworks.com>
2026-05-26 14:39:19 +0530

    working on join test cases

--- src/test/java/com/freshworks/core/processor/joins/TestLeftJoinService.java
+++ src/test/java/com/freshworks/core/processor/joins/TestLeftJoinService.java
@@ -1,8 +1,9 @@
 package com.freshworks.core.processor.joins;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doCallRealMethod;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,10 +12,16 @@ import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.freshworks.core.data.four_zero_zero.unit.processor.joins.assets.FbUserUsageAsset;
+import com.freshworks.core.data.four_zero_zero.unit.processor.joins.beans.FbApplicationBean;
+import com.freshworks.core.data.four_zero_zero.unit.processor.joins.beans.FbServicePrincipleBean;
 import com.freshworks.core.data.four_zero_zero.unit.processor.joins.beans.FbUsageBean;
 import com.freshworks.core.data.four_zero_zero.unit.processor.joins.beans.FbUserBean;
+import com.freshworks.core.processor.AbstractBean;
 import com.freshworks.core.processor.Annotations.FreshJoin;
+import com.freshworks.core.processor.joins.LeftJoinService;
 
 @SpringBootTest
 @EnabledIfSystemProperty(named = "spring.profiles.active", matches = ".*\\.unit\\..*")
@@ -56,6 +63,43 @@ public class TestLeftJoinService {
         String lookupFieldValueOfRightClass = leftJoinService.getLookupFieldValueOfRightClass(fbUsageBean, freshJoin);
         
         assertThat(lookupFieldValueOfRightClass, Matchers.is("user_id_654321"));
+
+    }
+
+    @Test
+    public void testCompareParent() throws Exception{
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        FbApplicationBean fbApplicationBean = new FbApplicationBean();
+        fbApplicationBean.setApplicationId("app_id_994433");
+        fbApplicationBean.setApplicationName("slack");
+
+        FbServicePrincipleBean fbServicePrincipleBean = new FbServicePrincipleBean();
+        fbServicePrincipleBean.setServicePrincipleId("sp_id_5575775");
+        fbServicePrincipleBean.setParentBean(objectMapper.convertValue(fbApplicationBean, JsonNode.class));
+
+
+        FbUserBean fbUserBean = new FbUserBean();
+        fbUserBean.setLastName("aggarwal");
+        fbUserBean.setFirstName("amit");
+        fbUserBean.setId("user_id_123456");
+        fbUserBean.setParentBean(objectMapper.convertValue(fbServicePrincipleBean, JsonNode.class));
+
+        FbUsageBean fbUsageBean = new FbUsageBean();
+        fbUsageBean.setCreatedAt("02-06-1989");
+        fbUsageBean.setUserId("user_id_654321");
+        fbUsageBean.setParentBean(objectMapper.convertValue(fbApplicationBean, JsonNode.class));
+
+        ArrayList<AbstractBean> list = new ArrayList<>();
+        list.add(0, fbUserBean);
+        HashMap<String, AbstractBean> unwrappedLeftClassMap = leftJoinService.unwrappedBeanToClassMap(list);
+        
+        list.add(0, fbUsageBean);
+        HashMap<String, AbstractBean> unwrappedRightClassMap = leftJoinService.unwrappedBeanToClassMap(list);
+
+        Boolean isEqual = leftJoinService.compareParent(unwrappedLeftClassMap, unwrappedRightClassMap);
+        assertThat(isEqual, Matchers.is(true));
 
     }
 }
