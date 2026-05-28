2e878dbb06d7280e469715a5682a9e9df1e4b9e2
aaggarwal_fwinc <amit.aggarwal@freshworks.com>
2026-05-28 12:06:09 +0530

    added test cases for asset bean dependency

--- src/test/java/com/freshworks/core/processor/TestAssetDependencyService.java
+++ src/test/java/com/freshworks/core/processor/TestAssetDependencyService.java
@@ -3,6 +3,7 @@ package com.freshworks.core.processor;
 import com.freshworks.core.data.four_zero_zero.unit.dag.assets.inner.TestInnerAsset;
 import com.freshworks.core.data.four_zero_zero.unit.dag.assets.inner.innermost.TestInnerMostAsset;
 import com.freshworks.core.data.four_zero_zero.unit.dag.assets.inner.innermost.TestInnerMostJoinedAsset;
+import com.freshworks.core.data.four_zero_zero.unit.processor.joins.assets.FbUserUsageAssetMultiFreshJoin;
 import com.freshworks.core.shared.MockFacadeSyncServiceContainer;
 import com.freshworks.core.shared.SyncServiceContainer;
 import com.google.common.collect.ImmutableListMultimap;
@@ -16,6 +17,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.doCallRealMethod;
+
+import java.util.List;
 
 @SpringBootTest
 @EnabledIfSystemProperty(named = "spring.profiles.active", matches = ".*\\.unit\\..*")
@@ -62,5 +65,25 @@ public class TestAssetDependencyService {
         assertThat(x.containsKey(TestInnerMostAsset.class.getName()), Matchers.is(true));
         assertThat(x.containsKey(TestInnerMostJoinedAsset.class.getName()), Matchers.is(true));
         assertThat(x.containsKey(TestInnerAsset.class.getName()), Matchers.is(true));
+    }
+
+    @Test
+    public void testWhenAssetHasMultipleFreshJoinAnnotationThenAssetBeanDependencyShouldNotCombineTwo() throws Exception{
+
+        ProcessorConfigService processorConfigService = mockFacadeProcessorConfigService
+                .getAssetLocation("com.freshworks.core.data." + releaseVersion + ".unit.processor.joins.assets")
+                .getBeanLocation("com.freshworks.core.data."+ releaseVersion + ".unit.processor.joins.beans")
+                .build();
+
+        AssetBeanDependencyService assetBeanDependencyService = mockFacadeAssetBeanDependencyService
+            .build();
+
+        doCallRealMethod().when(assetBeanDependencyService).scanner(anyString(), any());
+        doCallRealMethod().when(assetBeanDependencyService).findDependencyOfAsset(anyList(), any());
+        ImmutableListMultimap<String, String> x = assetBeanDependencyService.scanner("some-random-namespace", processorConfigService);
+        assertThat(x.containsKey(FbUserUsageAssetMultiFreshJoin.class.getName()), Matchers.is(true));
+        List<String> listOfBeanDependencies = x.get(FbUserUsageAssetMultiFreshJoin.class.getName());
+        assertThat(listOfBeanDependencies, Matchers.hasItems(Matchers.containsString("FbUserBean"), Matchers.containsString("FbGroupBean"), Matchers.containsString("FbUsageBean")));
+        
     }
 }
