91a577b31d8312468bcc84a0838ae708b464a389
aaggarwal_fwinc <amit.aggarwal@freshworks.com>
2026-06-08 13:52:47 +0530

    adding docker setup

---
+++ docker-setup-for-testing/third-party-setup/facebook_discovery_usecase/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FacebookDiscoveryUsecaseConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'facebook_discovery_usecase'
