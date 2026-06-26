4ad78c814a18c1f89f92d4eb85507504486ecebf
aaggarwal_fwinc <amit.aggarwal@freshworks.com>
2026-06-26 16:45:25 +0530

    tested hagrid in webserver mode also

--- src/main/java/com/freshworks/core/shared/infra/nitrite/NitriteFactory.java
+++ src/main/java/com/freshworks/core/shared/infra/nitrite/NitriteFactory.java
@@ -55,7 +55,6 @@ public class NitriteFactory {
 
                     }
                     else{
-                        
                         nitriteDb = Nitrite.builder()
                         .openOrCreate();
                     }
--- src/test/java/com/freshworks/core/performance/TestPerformance.java
+++ src/test/java/com/freshworks/core/performance/TestPerformance.java
@@ -4,7 +4,11 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.RepeatedTest;
@@ -14,8 +18,13 @@ import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.actuate.observability.AutoConfigureObservability;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.freshworks.core.data.four_five_zero.performance.fb.assets.FbUser;
+import com.freshworks.core.data.four_five_zero.performance.fb.steps.FbUserServer;
 import com.freshworks.core.shared.SyncServiceContainer;
+import com.freshworks.core.shared.consumer.ConsumerService;
 import com.freshworks.core.shared.sync.SyncService;
 import com.freshworks.core.shared.sync.SyncStatusService;
 import com.freshworks.core.shared.synchronizers.ServiceTree;
@@ -28,7 +37,12 @@ import com.google.common.collect.ImmutableMap;
 public class TestPerformance {
 
     @Autowired
+    ApplicationContext applicationContext;
+
+    @Autowired
     SyncService syncService;
+
+
     @Autowired
     private ServiceTree serviceTree;
 
@@ -100,5 +114,48 @@ public class TestPerformance {
         assertThat(syncStatusService.getTraverser_status(), Matchers.is(1));
         assertThat(syncStatusService.getProcessor_status(), Matchers.is(1));
         Thread.sleep(10000);
+    }
+
+
+    @Test
+    public void testWhenThousandAPIRequestComes() throws Exception {
+
+        CompletableFuture<Void> futureTasks[] = new CompletableFuture[1000];
+        ObjectMapper objectMapper = new ObjectMapper();
+        
+        for(int i=0; i<1000; i++){
+
+            CompletableFuture<Void> task = CompletableFuture.runAsync(() ->{
+
+                try{
+
+                    SyncService syncService = applicationContext.getBean(SyncService.class);
+                    ImmutableMap<String, String> x = ImmutableMap.<String, String>builder()
+                        .build();
+
+                    SyncServiceContainer syncServiceContainer = syncService.initSyncServiceContainer(UUID.randomUUID().toString(),  FbUserServer.class, x);
+                    syncService.startSync(syncServiceContainer);
+
+                    SyncStatusService syncStatusService = syncServiceContainer.getBean(SyncStatusService.class);
+                    syncStatusService.waitUntilSyncIsInProgress();
+                    ConsumerService consumerService = syncServiceContainer.getBean(ConsumerService.class);
+                    List<FbUser> fbUserList = consumerService.getAssetByAssetType(FbUser.class);
+                    System.out.println(objectMapper.writeValueAsString(fbUserList));
+                    assertThat(fbUserList.size(), Matchers.is(100));
+                    syncService.shutdown();
+                }
+                catch(Exception e){
+                    e.printStackTrace();
+                }
+
+            });
+
+            futureTasks[i] = task;
+        }
+
+        CompletableFuture<Void> allCompleted = CompletableFuture.allOf(futureTasks);
+        allCompleted.join();
+        System.out.println("All Completed");
+
     }
 }
